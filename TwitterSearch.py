36fe802b6495fcae0da85a9604be4fea095ad3dc
wattnotions <shaneormonde@gmail.com>
2015-09-03 11:27:44 -0700

    add search function
    search for delay keywords

---
+++ TwitterSearch.py
@@ -0,0 +1,14 @@
+import tweepy
+
+auth = tweepy.OAuthHandler('WzyxRNEEOfuzMJuXJsRiKwkTu', 'U18LqVCGXOvtV9y5KNWIcTg0E93Y5uxvZeer6VTe1gV3yaIpIz')
+auth.set_access_token('551032669-W6xZcJDSFIUq8uWPBp5y6ssFtpikVYQ3vTB6mpTu', 'qycG3wuqxQvsFVV17hu9G8awhBkFWEsY8CDLVyOjK4o50')
+
+api = tweepy.API(auth)
+
+#public_tweets = api.home_timeline()
+
+
+public_tweets  = api.user_timeline(id = 'IrishRail')
+#public_tweets = API.user_timeline(['15115986')
+for tweet in public_tweets:
+    repr(print(tweet.text))
